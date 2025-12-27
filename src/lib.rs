2517b2ff3ce780dddadf7ffa99d7a7baf2b55f89
Robin Adamietz <31573718+r0binius@users.noreply.github.com>
2025-12-27 19:31:16 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use nih_plug::{prelude::*, util::db_to_gain};
+use std::sync::Arc;
+
+struct MyPlugin {
+    params: Arc<PluginParams>,
+}
+
+#[derive(Params)]
+struct PluginParams {
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for MyPlugin {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(PluginParams::default()),
+        }
+    }
+}
+
+impl Default for PluginParams {
+    fn default() -> Self {
+        Self {
+            gain: FloatParam::new(
+                "Gain",
+                0.0,
+                FloatRange::Linear {
+                    min: -10.0,
+                    max: 10.0,
+                },
+            )
+            .with_step_size(0.1)
+            .with_smoother(SmoothingStyle::Linear(50.0))
+            .with_unit(" dB"),
+        }
+    }
+}
+
+impl Plugin for MyPlugin {
+    const NAME: &'static str = "My Plugin";
+    const VENDOR: &'static str = "SteckTech";
+    const URL: &'static str = "https://steck.tech";
+    const EMAIL: &'static str = "info@steck.tech";
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(2),
+            main_output_channels: NonZeroU32::new(2),
+            aux_input_ports: &[],
+            aux_output_ports: &[],
+            names: PortNames::const_default(),
+        },
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(1),
+            main_output_channels: NonZeroU32::new(1),
+            ..AudioIOLayout::const_default()
+        },
+    ];
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+    type SysExMessage = ();
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        _buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        true
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel_samples in buffer.iter_samples() {
+            let gain = self.params.gain.smoothed.next();
+            let gain = db_to_gain(gain);
+
+            for sample in channel_samples {
+                *sample *= gain;
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+
+    fn reset(&mut self) {}
+
+    // This can be used for cleaning up special resources like socket connections whenever the
+    // plugin is deactivated. Most plugins won't need to do anything here.
+    fn deactivate(&mut self) {}
+}
+
+impl ClapPlugin for MyPlugin {
+    const CLAP_ID: &'static str = "com.stecktech.myplug";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("An example plugin");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+    const CLAP_FEATURES: &'static [ClapFeature] = &[
+        ClapFeature::AudioEffect,
+        ClapFeature::Stereo,
+        ClapFeature::Mono,
+        ClapFeature::Utility,
+    ];
+}
+
+impl Vst3Plugin for MyPlugin {
+    const VST3_CLASS_ID: [u8; 16] = *b"MStecktechPlugin";
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
+}
+
+nih_export_clap!(MyPlugin);
+nih_export_vst3!(MyPlugin);
